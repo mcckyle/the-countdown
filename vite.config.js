68fda7eaa16f4069e73c73d148ba0d49e4329556
Kyle McColgan <kdmbus21@gmail.com>
2025-09-15 16:23:06 -0500

    vite.config.js: Include the correct base path for the GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+//Filename: vite.config.js
+//Author: Kyle McColgan
+//Date: 15 September 2025
+//Description: This file contains the Vite config for the Countdown React project.
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/the-countdown/',
 })
