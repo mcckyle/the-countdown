78422387c57391dfe520b0aff235e372d6fe06ef
Kyle McColgan <kdmbus21@gmail.com>
2026-01-19 21:51:08 -0600

    Header.jsx: Replace the plain text apostrophe with the corresponding HTML character entry.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 //Filename: Header.jsx
 //Author: Kyle McColgan
-//Date: 9 January 2026
+//Date: 19 January 2026
 //Description: This file contains the Header component for the React Countdown project.
 
 import "./Header.css";
@@ -11,7 +11,7 @@ function Header()
     <header className="header">
       <h1 className="header-title">
         <span className="header-accent" aria-hidden="true">❤️</span>
-         Valentine's&nbsp;Day&nbsp;Countdown
+         Valentine&rsquo;s&nbsp;Day&nbsp;Countdown
       </h1>
 
       <p className="header-subtitle">
